2876e3c2509b22b2b75996265ef832a64feedfe3
Andre Marques <andremarques94@gmail.com>
2022-11-21 16:29:14 +0000

    created the first routes for offers

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
