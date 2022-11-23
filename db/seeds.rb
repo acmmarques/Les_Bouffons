d795b4708c0eddbd8b6df4a381af6f396927653b
Manuel Kneib <manuel.kneib@gmail.com>
2022-11-23 12:30:24 +0000

    seed images for 10 clowns and show them in index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,27 +7,49 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require "faker"
+require "open-uri"
 
 puts "Destroying all Clowns"
 Offer.destroy_all
 User.destroy_all
 user = User.create(name: "seed user", email: "sample@gmail.com", password: "1234567")
+clown1 = ["https://res.cloudinary.com/darkysplk/image/upload/v1669201977/Les%20Bouffons/ana_1_tmuaqq.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201977/Les%20Bouffons/ana_3_ukxda2.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201977/Les%20Bouffons/ana_4_stj8ue.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201978/Les%20Bouffons/ana_5_twcw1l.jpg"]
+clown2 = ["https://res.cloudinary.com/darkysplk/image/upload/v1669201977/Les%20Bouffons/andre_1_elgl5d.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201978/Les%20Bouffons/andre_2_gkuv1i.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201979/Les%20Bouffons/andre_3_kmrr0y.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201979/Les%20Bouffons/andre_4_rthh1k.jpg"]
+clown3 = ["https://res.cloudinary.com/darkysplk/image/upload/v1669201979/Les%20Bouffons/carlos_1_ooooon.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201979/Les%20Bouffons/carlos_2_oarcdv.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201979/Les%20Bouffons/carlos_3_cj1enb.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201980/Les%20Bouffons/carlos_4_mmhaaf.jpg"]
+clown4 = ["https://res.cloudinary.com/darkysplk/image/upload/v1669201980/Les%20Bouffons/catarina_1_rao7uv.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201980/Les%20Bouffons/catarina_3_jmayjd.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201980/Les%20Bouffons/catarina_4_pk60wc.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201981/Les%20Bouffons/catarina_5_shqz6e.jpg"]
+clown5 = ["https://res.cloudinary.com/darkysplk/image/upload/v1669201981/Les%20Bouffons/dal_1_m0hhnx.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201981/Les%20Bouffons/dal_3_vtvasp.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201982/Les%20Bouffons/dal_4_cxjquz.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201982/Les%20Bouffons/dal_5_bwr0cn.jpg"]
+clown6 = ["https://res.cloudinary.com/darkysplk/image/upload/v1669201982/Les%20Bouffons/danilo_1_eamina.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201982/Les%20Bouffons/danilo_3_qfjvud.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201983/Les%20Bouffons/danilo_4_ddnkb7.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201983/Les%20Bouffons/danilo_5_ttarq6.jpg"]
+clown7 = ["https://res.cloudinary.com/darkysplk/image/upload/v1669201983/Les%20Bouffons/gabriel_1_lqwouh.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201983/Les%20Bouffons/gabriel_2_fzflqk.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201978/Les%20Bouffons/gabriel_3_fkflkn.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201978/Les%20Bouffons/gabriel_5_bkl6fy.jpg"]
+clown8 = ["https://res.cloudinary.com/darkysplk/image/upload/v1669201978/Les%20Bouffons/gerardo_1_uwfylo.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201978/Les%20Bouffons/gerardo_3_xt2400.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201978/Les%20Bouffons/gerardo_5_w9du3r.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201978/Les%20Bouffons/gerardo_6_vjgwrd.jpg"]
+clown9 = ["https://res.cloudinary.com/darkysplk/image/upload/v1669201979/Les%20Bouffons/havard_1_wiqizx.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201979/Les%20Bouffons/havard_3_t3peva.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201979/Les%20Bouffons/havard_4_hkvhwm.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201980/Les%20Bouffons/havard_5_qf9kfc.jpg"]
+clown10 = ["https://res.cloudinary.com/darkysplk/image/upload/v1669201980/Les%20Bouffons/ines_1_cknp1f.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201980/Les%20Bouffons/ines_2_i84jqg.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201980/Les%20Bouffons/ines_4_bm0npe.jpg", "https://res.cloudinary.com/darkysplk/image/upload/v1669201980/Les%20Bouffons/ines_5_figrws.jpg"]
+clown_images = []
+clown_images.push(clown1).push(clown2).push(clown3).push(clown4).push(clown5).push(clown6).push(clown7).push(clown8).push(clown9).push(clown10)
+
+
 
 puts "Creating sample Clowns"
 
-tricks = ["juggle", "handstand", "balloons", "face paint", "singing", "dad jokes", "card tricks", "magic tricks", "instrument", "dance", "acrobatics", "annoy"]
+tricks = ["juggle", "handstand", "balloons", "face-paint", "singing", "dad-jokes", "card-tricks", "magic-tricks", "instrument", "dance", "acrobatics", "annoy"]
+
+i = 0
 
 10.times do
   clown = Offer.new(
     name: Faker::TvShows::SouthPark.character,
-    features: tricks.sample + ", " + tricks.sample + ", " + tricks.sample,
+    features: tricks.sample + " " + tricks.sample + " " + tricks.sample,
     details: Faker::Quotes::Shakespeare.romeo_and_juliet_quote,
     price: rand(50..150)
   )
+  for image in clown_images[i]
+    file = URI.open(image)
+    clown.photos.attach(io: file, filename: image, content_type: "image/png")
+  end
 
   clown.user = user
   clown.save
   puts "Clown #{clown.name} - #{clown.save}"
+  i += 1
 end
 
 puts "The Clowns were created successfully"
