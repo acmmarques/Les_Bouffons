1834b9db28379598219bd391d7df534b2591e9df
Loic Thieffry <thieffry.loic@gmail.com>
2022-11-24 15:30:20 +0000

    finished-log-in-and-name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,3 +10,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
+
--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,4 +1,5 @@
 class OffersController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
   before_action :set_clown, only: %i[ show edit update destroy ]
 
   def index
